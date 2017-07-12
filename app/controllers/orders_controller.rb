f6c77c79d8f16bb5f3643fcd8992f976419a3d3f
Laura <laura.ciro@yuxiglobal.com>
2017-07-12 14:04:31 -0500

    pusher implementation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,10 @@ class OrdersController < ApplicationController
   def index
     render json: { data: Order.all }
   end
+ def pusher_prueba
+    Order.pusher_prueba
+    puts '#'*60
+    puts 'SIIIII'
+    render json: { requested: true }
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,39 @@
 class Order < ApplicationRecord
   has_many :positions
+  def self.pusher_prueba
+    Pusher.trigger('test_channel_ng', 'assign_position', {:status => true,
+       :data =>[{
+                    type: "load",
+                    color: "MAGNETA",
+                    animation: "",
+                    order: {
+                        id: "5",
+                        deliver_order_number: "100108633",
+                        local_status: 2,
+                        cook:  {
+                           code: "2",
+                           name: "Maria Rios",
+                           used: true
+                        },
+                        created_at: "2017-06-29 07:10:09",
+                        products: [
+                            {
+                              quantity: 5,
+                              name: "Kroger Meal Deal"
+                            },
+                            {
+                              quantity: 2,
+                              name: "Italian Sausage"
+                            },
+                            {
+                              quantity: 5,
+                              name: "beer"
+                            }
+                        ],
+                        positions: ["5"]
+                    }
+                }
+               ]
+      })
+  end
 end
